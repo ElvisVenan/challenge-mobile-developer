d491b5e6a2335e110873b7b68b582aa978935134
Elvis <elvis@ezsolucoes.com.br>
2023-12-03 00:37:10 -0300

    feature(AppDimens): cria o arquivo de dimensoes.

---
+++ lib/utils/const/app_dimens.dart
@@ -0,0 +1,29 @@
+class AppDimens {
+
+  static const double zeroDimension = 0.0;
+
+  //Margin
+  static const double tinyMarginDimension = 4.0;
+  static const double smallMarginDimension = 8.0;
+  static const double defaultMarginDimension = 16.0;
+  static const double bigMarginDimension = 20.0;
+  static const double hugeMarginDimension = 28.0;
+  static const double veryBigMarginDimension = 32.0;
+  static const double largeMarginDimension = 40.0;
+  static const double ampleMarginDimension = 48.0;
+  static const double veryLargeMarginDimension = 64.0;
+  static const double immenseMarginDimension = 120.0;
+
+  //fonts
+  static const double fontSizeTinyDimension = 10;
+  static const double fontSizeMiniDimension = 12;
+  static const double fontSizeCompactDimension = 13;
+  static const double fontSizeSmallDimension = 14;
+  static const double fontSizeMediumDimension = 16;
+  static const double fontSizeBigDimension = 18;
+  static const double fontSizeLargeDimension = 20;
+  static const double fontSizeHugeDimension = 22;
+  static const double fontSizeGiantDimension = 24;
+
+
+}
