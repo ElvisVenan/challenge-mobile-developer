cb13ee22870e2aa0a724c703bf329be4f2311051
Elvis <elvis@ezsolucoes.com.br>
2023-12-04 18:35:41 -0300

    feature(AppColors): cria as cores.
    - oceanBlueColor
    - softSkyBlueColor
    - grayColor

--- lib/const/app_colors.dart
+++ lib/const/app_colors.dart
@@ -4,5 +4,8 @@ class AppColors {
 
   static const Color pureWhiteColor = Color(0xffFFFFFF);
   static const Color pureBlackColor = Color(0xFF000000);
+  static const Color oceanBlueColor = Color(0xFF116281);
+  static const Color softSkyBlueColor = Color(0xFFA8C4D0);
+  static const Color grayColor = Color(0xFF808080);
 
 }
