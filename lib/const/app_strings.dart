76fb3ef2638abb135f65acaf4274f87a44a4eb43
Elvis <elvis@ezsolucoes.com.br>
2023-12-04 18:38:14 -0300

    feature(AppStrings): cria a classe de string.

---
+++ lib/const/app_strings.dart
@@ -0,0 +1,24 @@
+class AppStrings {
+
+  // Erros genéricos
+  static const String genericErrorTitle = 'Algo deu errado';
+  static const String genericErrorMessage =
+      'Ocorreu um erro inesperado. Por favor, tente novamente mais tarde ou entre em contato conosco para relatar o problema.';
+  static const String serverErrorMessage =
+      'Ocorreu um erro no processamento da sua requisição. Por favor, tente novamente mais tarde ou entre em contato conosco para relatar o problema.';
+  static const String serverErrorCausedBy =
+      'Erro ao fazer a chamada de um serviço na API.';
+  static const String reautenticationNeededErrorMessage =
+      'Erro ao efetuar o Login.\nPor favor, verifique os dados digitados e repita a operação.';
+  static const String unauthenticatedUserErrorCausedBy =
+      'Usuário não autenticado.';
+
+  static const String usernameString = 'Usuário';
+  static const String passwordString = 'Senha';
+  static const String forgotUsernameString = 'Esqueci o usuário';
+  static const String forgotPasswordString = 'Esqueci a senha';
+  static const String registerString = 'Cadastrar';
+  static const String loginString = 'Entrar';
+  static const String orString = 'Ou';
+
+}
