fa1c20d782d383e8f01e55c4eb9d84de7aa2d161
Elvis <elvis@ezsolucoes.com.br>
2023-12-05 17:43:51 -0300

    feature(AuthenticationService): adiciona o metodo getUser.

--- lib/services/authentication_service/authentication_service.dart
+++ lib/services/authentication_service/authentication_service.dart
@@ -1,13 +1,16 @@
 import 'package:dartz/dartz.dart';
 
-import '../../models/params/authentication_params/authentication_params.dart';
 import '../../utils/http/errors/application_error.dart';
 
+import '../../models/params/authentication_params/authentication_params.dart';
 import '../../models/authentication_model/authentication_model.dart';
 
 abstract class AuthenticationService {
 
   @override
-  Future<Either<ApplicationError, AuthenticationModel>> authentication(AuthenticationParams? authentication);
+  Future<Either<ApplicationError, AuthenticationModel>> createUser(AuthenticationParams? authentication);
+
+  @override
+  Future<Either<ApplicationError, AuthenticationModel>> getUser(int id);
 
 }
