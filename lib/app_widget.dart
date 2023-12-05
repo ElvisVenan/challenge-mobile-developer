1450bbed8d4b7187719db728403633c89e9dc552
Elvis <elvis@ezsolucoes.com.br>
2023-12-05 00:22:18 -0300

    feature(AppWidget): adiciona o theme com cor oceanBlueColor.

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
+import 'const/app_colors.dart';
+
 class AppWidget extends StatelessWidget {
   const AppWidget({Key? key}) : super(key: key);
 
@@ -8,6 +10,11 @@ class AppWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: AppColors.oceanBlueColor,
+          )
+      ),
       routeInformationParser: Modular.routeInformationParser,
       routerDelegate: Modular.routerDelegate,
     );
