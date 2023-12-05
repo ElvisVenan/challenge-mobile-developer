b732db1ec9073dd0d8c8d8a490c3fcda8a5316e9
Elvis <elvis@ezsolucoes.com.br>
2023-12-05 17:44:41 -0300

    feature(AuthenticationServiceImpl): adiciona o metodo getUser.

--- lib/services/authentication_service/authentication_service_Implementation.dart
+++ lib/services/authentication_service/authentication_service_Implementation.dart
@@ -1,6 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:http/http.dart' as http;
 
+import '../../const/app_strings.dart';
 import '../../utils/http/errors/application_error.dart';
 import '../../utils/http/errors/authentication_error.dart';
 import '../../utils/http/mixins/uri_builder.dart';
@@ -19,7 +20,7 @@ class AuthenticationServiceImpl
   final http.Client httpClient;
 
   @override
-  Future<Either<ApplicationError, AuthenticationModel>> authentication(
+  Future<Either<ApplicationError, AuthenticationModel>> createUser(
       AuthenticationParams? authentication) async {
     final String url = mountUrl(
       AppEndpoints.baseUrlProtocolWithSecurity,
@@ -37,34 +38,65 @@ class AuthenticationServiceImpl
       );
 
       if (response.statusCode == 201) {
-
         final data = response.body;
         return Right(AuthenticationModel.fromJson(data));
-
       } else if (response.statusCode == 400) {
-
         return Left(AuthenticationError(
-            friendlyMessage: 'Falha na autenticação',
-            causedBy: 'Cliente',
-            additionalInfo: 'Código de status: ${response.statusCode}'));
-
+            friendlyMessage: AppStrings.authenticationFailureMessageString,
+            causedBy: AppStrings.clientMessageString,
+            additionalInfo:
+                '${AppStrings.statusCodeMessageString}: ${response.statusCode}'));
       } else if (response.statusCode == 500) {
-
         return Left(AuthenticationError(
-            friendlyMessage: 'Erro no serviço',
-            causedBy: 'Servidor',
-            additionalInfo: 'Código de status: ${response.statusCode}'));
-
+            friendlyMessage: AppStrings.serviceErrorMessageString,
+            causedBy: AppStrings.serviceMessageString,
+            additionalInfo:
+                '${AppStrings.statusCodeMessageString}: ${response.statusCode}'));
       } else {
-
-        return Left(GenericError(causedBy: "Desconhecida"));
-
+        return Left(
+            GenericError(causedBy: AppStrings.unknownCauseMessageString));
       }
     } catch (e) {
-
       return Left(AuthenticationError(
           friendlyMessage: e.toString(), additionalInfo: e.toString()));
+    }
+  }
 
+  @override
+  Future<Either<ApplicationError, AuthenticationModel>> getUser(int id) async {
+    final String url = mountUrl(
+      AppEndpoints.baseUrlProtocolWithSecurity,
+      AppEndpoints.mainBaseUrl,
+      AppEndpoints.authenticateEndpoint,
+    );
+
+    try {
+      final response = await httpClient.get(
+        Uri.parse("$url/$id"),
+      );
+
+      if (response.statusCode == 200) {
+        final data = response.body;
+        return Right(AuthenticationModel.fromJson(data));
+      } else if (response.statusCode == 400) {
+        return Left(AuthenticationError(
+            friendlyMessage: AppStrings.authenticationFailureMessageString,
+            causedBy: AppStrings.clientMessageString,
+            additionalInfo:
+                '${AppStrings.statusCodeMessageString}: ${response.statusCode}'));
+      } else if (response.statusCode == 500) {
+        return Left(AuthenticationError(
+            friendlyMessage: AppStrings.serviceErrorMessageString,
+            causedBy: AppStrings.serviceMessageString,
+            additionalInfo:
+                '${AppStrings.statusCodeMessageString}: ${response.statusCode}'));
+      } else {
+        return Left(
+            GenericError(causedBy: AppStrings.unknownCauseMessageString));
+      }
+    } catch (e) {
+      return Left(AuthenticationError(
+          friendlyMessage: e.toString(), additionalInfo: e.toString()));
     }
   }
 }
