cd8022c3fa58a2652eb3b43b54139bb5bc378951
Elvis <elvis@ezsolucoes.com.br>
2023-12-04 23:19:58 -0300

    feature(AppEndpoints): cria a classe dos endpoints.

---
+++ lib/services/endpoints/app_endpoints.dart
@@ -0,0 +1,10 @@
+class AppEndpoints {
+
+  static const mainBaseUrl = '653c0826d5d6790f5ec7c664.mockapi.io';
+  static const String baseUrlProtocolWithSecurity = 'https';
+  static const String baseUrlProtocol = 'http';
+
+  //Authentication
+  static const String authenticateEndpoint = 'api/v1/login';
+
+}
