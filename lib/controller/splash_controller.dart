a6a30c233b3c584df263e7456a7c006aa661223d
Elvis <elvis@ezsolucoes.com.br>
2023-12-05 17:49:27 -0300

    feature(SplashController): cria o controller da SplashPage.
    feature(SplashController): adiciona a logica verificar se usuario esta autenticado.

---
+++ lib/controller/splash_controller.dart
@@ -0,0 +1,35 @@
+import 'dart:async';
+
+import 'package:challenge_mobile_developer/pages/home_page/home_page.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:mobx/mobx.dart';
+
+import '../controller/authentication_controller.dart';
+import '../pages/login_page/login_page.dart';
+import '../services/local_service/i_local_service.dart';
+import '../services/local_service/local_service_impl.dart';
+
+part 'splash_controller.g.dart';
+
+class SplashController = _SplashController with _$SplashController;
+
+abstract class _SplashController with Store {
+  @action
+  Future<void> checkLoggedInUser() async {
+    final localServiceController = Modular.get<ILocalService>();
+
+    final authController = Modular.get<AuthenticationController>();
+    var id = await localServiceController.get(LocalServiceImpl.ID_KEY);
+
+    if (id != null) {
+      await authController.login(int.parse(id));
+      if (id == (authController.authenticationResponse?.id ?? "")) {
+        HomePage.navigate();
+      } else {
+        LoginPage.navigate();
+      }
+    } else {
+      LoginPage.navigate();
+    }
+  }
+}
