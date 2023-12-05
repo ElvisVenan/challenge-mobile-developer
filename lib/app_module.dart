27b477e583cc03d64a3e40ab820375c6740af678
Elvis <elvis@ezsolucoes.com.br>
2023-12-05 17:05:17 -0300

    feature(AppModule): adiciona a injecao de dependencia do LocalService.

--- lib/app_module.dart
+++ lib/app_module.dart
@@ -3,17 +3,21 @@ import 'package:http/http.dart' as http;
 
 import 'const/app_routes.dart';
 
+import '../pages/home_page/home_page.dart';
+import '../services/local_service/i_local_service.dart';
 import '../controller/authentication_controller.dart';
 import '../pages/login_page/login_page.dart';
 import '../services/authentication_service/authentication_service.dart';
 import '../services/authentication_service/authentication_service_Implementation.dart';
+import '../services/local_service/local_service_impl.dart';
 import '../pages/splash_page/splash_page.dart';
-
+import 'controller/splash_controller.dart';
 
 class AppModule extends Module {
   @override
   List<Bind> get binds => [
         ...servicesRemoteBinds,
+        ...servicesLocalBinds,
         ...controllersBinds,
       ];
 
@@ -23,17 +27,24 @@ class AppModule extends Module {
             (i) => AuthenticationServiceImpl(i())),
       ];
 
+  List<Bind> get servicesLocalBinds => [
+        Bind.lazySingleton<ILocalService>((i) => LocalServiceImpl()),
+      ];
+
   List<Bind> get controllersBinds => [
-    Bind.factory((i) => AuthenticationController()),
-  ];
+        Bind.factory((i) => AuthenticationController()),
+        Bind.factory((i) => SplashController()),
+      ];
 
   @override
   List<ModularRoute> get routes => [
         ChildRoute(AppRoutes.appDefaultPage,
-            child: (context, args) => const LoginPage()),
+            child: (context, args) => const SplashPage()),
         ChildRoute(AppRoutes.appSplashPage,
             child: (context, args) => const SplashPage()),
         ChildRoute(AppRoutes.appLoginPage,
             child: (context, args) => const LoginPage()),
+        ChildRoute(AppRoutes.appHomePage,
+            child: (context, args) => const HomePage()),
       ];
 }
