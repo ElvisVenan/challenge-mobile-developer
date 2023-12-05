50061eb2e10c711fc8e404eee1a9a6256f56fdf3
Elvis <elvis@ezsolucoes.com.br>
2023-12-05 19:42:59 -0300

    feature(HelperPage): cria a pagina.

---
+++ lib/pages/home_page/components/bottom_navigation_pages/helper_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HelperPage extends StatelessWidget {
+  const HelperPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("Conteúdo da Página Ajuda"),
+    );
+  }
+}
