685b7525d0e8444a1f982c515c8f146b62cb6990
Elvis <elvis@ezsolucoes.com.br>
2023-12-04 23:22:52 -0300

    feature(AppModule): adiciona a classe AuthenticationServiceImpl.
    - Realiza a injecao de dependencia
    
    feature(AppModule): adiciona AuthenticationController com factory.

--- lib/app_module.dart
+++ lib/app_module.dart
@@ -1,9 +1,14 @@
-import 'package:challenge_mobile_developer/pages/login_page/login_page.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:http/http.dart' as http;
 
-import '../pages/splash_page/splash_page.dart';
 import 'const/app_routes.dart';
+
+import '../controller/authentication_controller.dart';
+import '../pages/login_page/login_page.dart';
+import '../services/authentication_service/authentication_service.dart';
+import '../services/authentication_service/authentication_service_Implementation.dart';
+import '../pages/splash_page/splash_page.dart';
+
 
 class AppModule extends Module {
   @override
@@ -14,9 +19,13 @@ class AppModule extends Module {
 
   List<Bind> get servicesRemoteBinds => [
         Bind.lazySingleton((i) => http.Client()),
+        Bind.lazySingleton<AuthenticationService>(
+            (i) => AuthenticationServiceImpl(i())),
       ];
 
-  List<Bind> get controllersBinds => [];
+  List<Bind> get controllersBinds => [
+    Bind.factory((i) => AuthenticationController()),
+  ];
 
   @override
   List<ModularRoute> get routes => [
