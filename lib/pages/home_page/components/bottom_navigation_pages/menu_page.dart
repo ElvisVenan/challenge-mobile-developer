421b7bdccb16ebcf8df80d4eb164c553cbe768e2
Elvis <elvis@ezsolucoes.com.br>
2023-12-06 18:09:34 -0300

    feature(MenuPage): adiciona os atributos onEdit e onDelete.

--- lib/pages/home_page/components/bottom_navigation_pages/menu_page.dart
+++ lib/pages/home_page/components/bottom_navigation_pages/menu_page.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 
+import '../../../../const/app_strings.dart';
+
 import '../../../../models/student_models/student_model.dart';
 import '../../../../widgets/container_widgets/card_with_text_and_icon_widget.dart';
 
 class MenuPage extends StatelessWidget {
   final List<StudentModel> student;
+  final Function() onEdit;
+  final Function(int studentId) onDelete;
 
-  const MenuPage({super.key, required this.student});
+  const MenuPage(
+      {super.key,
+      required this.student,
+      required this.onEdit,
+      required this.onDelete});
 
   @override
   Widget build(BuildContext context) {
@@ -17,11 +25,12 @@ class MenuPage extends StatelessWidget {
               itemCount: student.length,
               itemBuilder: (context, i) {
                 return CardWithTextAndIconWidget(
-                    title: student[i].name,
-                    subtitle1: student[i].academicRecord,
-                    subtitle2: "CPF: ${student[i].cpf}",
-                    onEdit: () {},
-                    onDelete: () {});
+                  title: student[i].name,
+                  subtitle1: student[i].academicRecord,
+                  subtitle2: "${AppStrings.cpfString}: ${student[i].cpf}",
+                  onEdit: onEdit,
+                  onDelete: () => onDelete(int.parse(student[i].id)),
+                );
               }),
         ),
       ],
