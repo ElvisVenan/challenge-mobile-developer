a6b134d65efe60d7b3d023bf360c2698bf08be77
Elvis <elvis@ezsolucoes.com.br>
2023-12-06 17:54:26 -0300

    feature(AppRoutes): adiciona a rota appStudentRegistrationPage.

--- lib/const/app_routes.dart
+++ lib/const/app_routes.dart
@@ -4,5 +4,6 @@ class AppRoutes{
   static const String appSplashPage = '/splash';
   static const String appLoginPage = '/login';
   static const String appHomePage = '/home';
+  static const String appStudentRegistrationPage = '/studentRegistration';
 
 }
