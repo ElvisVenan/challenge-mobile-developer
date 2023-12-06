4f545c86fdbe1016e53b9a5c06fffff54015876e
Elvis <elvis@ezsolucoes.com.br>
2023-12-05 22:42:18 -0300

    feature(HomeController): adiciona o metodo getStudents.

--- lib/controller/home_controller.dart
+++ lib/controller/home_controller.dart
@@ -1,4 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 import 'package:mobx/mobx.dart';
+
+import '../utils/show_message.dart';
+
+import '../models/student_models/student_model.dart';
+import '../services/remote_database/academic_record_service/student_service.dart';
 
 part 'home_controller.g.dart';
 
@@ -6,10 +13,39 @@ class HomeController = _HomeControllerBase with _$HomeController;
 
 abstract class _HomeControllerBase with Store {
   @observable
+  bool isLoading = false;
+
+  @observable
   int currentIndex = 0;
+
+  @observable
+  List<StudentModel> studentModel = [];
 
   @action
   void changeTabIndex(int index) {
     currentIndex = index;
+  }
+
+  @observable
+  String errorMessage = '';
+
+  @action
+  Future<void> getStudents(BuildContext context) async {
+    isLoading = true;
+    final studentController = Modular.get<StudentService>();
+
+    final students = await studentController.getStudents();
+
+    students.fold((error) {
+      errorMessage = error.friendlyMessage;
+      isLoading = false;
+      if (errorMessage.isNotEmpty) {
+        ShowMessage.showErrorMessage(context, errorMessage);
+      }
+      isLoading = false;
+    }, (success) {
+      studentModel = success;
+      isLoading = false;
+    });
   }
 }
