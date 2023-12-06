1c37ef2317d2737eb850a58892da8f328b379e74
Elvis <elvis@ezsolucoes.com.br>
2023-12-06 18:00:22 -0300

    feature(AppStrings): adiciona as strings de student registration page.

--- lib/const/app_strings.dart
+++ lib/const/app_strings.dart
@@ -1,6 +1,6 @@
 class AppStrings {
 
-  // Erros
+  // Erros API
   static const String authenticationFailureMessageString = 'Falha na autenticação';
   static const String clientMessageString = 'Cliente';
   static const String statusCodeMessageString = 'Código de status';
@@ -9,7 +9,6 @@ class AppStrings {
   static const String unknownCauseMessageString = 'Causa desconhecida';
   static const String genericErrorMessage =
       'Ocorreu um erro inesperado. Por favor, tente novamente mais tarde ou entre em contato conosco para relatar o problema.';
-
 
   static const String usernameString = 'Usuário';
   static const String passwordString = 'Senha';
@@ -24,4 +23,14 @@ class AppStrings {
   static const String menuString = 'Menu';
   static const String notificationString = 'Notificação';
   static const String profileString = 'Perfil';
+  static const String fillInTheRequiredFieldsString = 'Este campo é obrigatório. Por favor, preencha antes de continuar!';
+  static const String studentNameString = 'Nome do aluno';
+  static const String birthdateString = 'Data de nascimento';
+  static const String cpfString = 'CPF';
+  static const String academicRecordString = 'Registro acadêmico';
+  static const String emailString = 'E-mail';
+  static const String accessCredentialsString = 'Dados de acesso';
+  static const String generalDataString = 'Dados gerais';
+  static const String addString = 'Adicionar';
+
 }
