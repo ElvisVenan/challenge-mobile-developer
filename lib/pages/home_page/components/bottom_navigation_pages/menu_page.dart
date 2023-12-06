d746afed55c6cbe6ee39310eb9f7184fc6ee0090
Elvis <elvis@ezsolucoes.com.br>
2023-12-05 22:45:04 -0300

    feature(MenuPage): adiciona os cards a pagina.

--- lib/pages/home_page/components/bottom_navigation_pages/menu_page.dart
+++ lib/pages/home_page/components/bottom_navigation_pages/menu_page.dart
@@ -1,12 +1,30 @@
 import 'package:flutter/material.dart';
 
+import '../../../../models/student_models/student_model.dart';
+import '../../../../widgets/container_widgets/card_with_text_and_icon_widget.dart';
+
 class MenuPage extends StatelessWidget {
-  const MenuPage({super.key});
+  final List<StudentModel> student;
+
+  const MenuPage({super.key, required this.student});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Conteúdo da Página Inicial"),
+    return Column(
+      children: [
+        Expanded(
+          child: ListView.builder(
+              itemCount: student.length,
+              itemBuilder: (context, i) {
+                return CardWithTextAndIconWidget(
+                    title: student[i].name,
+                    subtitle1: student[i].academicRecord,
+                    subtitle2: "CPF: ${student[i].cpf}",
+                    onEdit: () {},
+                    onDelete: () {});
+              }),
+        ),
+      ],
     );
   }
 }
