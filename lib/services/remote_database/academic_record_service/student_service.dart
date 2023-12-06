6a5e93aeeafedccb7fd4f0a76d3fbdae4446dc32
Elvis <elvis@ezsolucoes.com.br>
2023-12-05 22:40:01 -0300

    feature(StudentService): cria a da abstracao de student service.

---
+++ lib/services/remote_database/academic_record_service/student_service.dart
@@ -0,0 +1,20 @@
+import 'package:dartz/dartz.dart';
+
+import '../../../models/student_models/student_model.dart';
+import '../../../utils/http/errors/application_error.dart';
+
+import '../../../models/params/academic_record_params/student_params.dart';
+
+abstract class StudentService {
+
+  Future<Either<ApplicationError, StudentModel>> createStudent(StudentParams academicRecord);
+
+  Future<Either<ApplicationError, List<StudentModel>>> getStudents();
+
+  Future<Either<ApplicationError, StudentModel>> deleteStudent(int id);
+
+  Future<Either<ApplicationError, StudentModel>> getStudentById(int id);
+
+  Future<Either<ApplicationError, StudentModel>> updateStudent(StudentParams academicRecord);
+
+}
