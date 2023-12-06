62dab1630b9b4505dca90ac1f7eca6ced52aa328
Elvis <elvis@ezsolucoes.com.br>
2023-12-05 22:40:56 -0300

    feature(StudentParams): cria o parametro StudentParams.

---
+++ lib/models/params/academic_record_params/student_params.dart
@@ -0,0 +1,17 @@
+class StudentParams {
+  final String id;
+  final String ra;
+  final String name;
+  final String email;
+  final String dateOfBirth;
+  final String cpf;
+
+  StudentParams({
+    required this.id,
+    required this.ra,
+    required this.name,
+    required this.email,
+    required this.dateOfBirth,
+    required this.cpf,
+  });
+}
