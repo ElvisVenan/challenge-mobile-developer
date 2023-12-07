d7a3904998f4a6e4b39c510f3d3a5f7438365e5f
Elvis <elvis@ezsolucoes.com.br>
2023-12-07 00:54:28 -0300

    feature(StudentRegistrationPage): adiciona os argumentos passado pela home page.

--- lib/pages/student_registration_page/student_registration_page.dart
+++ lib/pages/student_registration_page/student_registration_page.dart
@@ -11,6 +11,7 @@ import '../../const/app_text_styles.dart';
 import '../../utils/text_field_validator.dart';
 
 import '../../pages/home_page/home_page.dart';
+import '../../models/student_models/student_model.dart';
 import '../../widgets/circular_progress_widgets/circular_progress_white_color_widget.dart';
 import '../../widgets/app_bar_widgets/app_bar_ocean_blue_color_widget.dart';
 import '../../widgets/button_widgets/rounded_button_widget.dart';
@@ -18,13 +19,15 @@ import '../../widgets/text_field_widgets/rectangular_text_field_widget.dart';
 import '../../../controller/student_registration_controller.dart';
 
 class StudentRegistrationPage extends StatefulWidget {
+
   const StudentRegistrationPage({super.key});
 
   static const String routePath = AppRoutes.appStudentRegistrationPage;
 
   static void navigate() => Modular.to.navigate(routePath);
 
-  static void push() => Modular.to.pushNamed(routePath);
+  static void push(StudentModel? args) =>
+      Modular.to.pushNamed(routePath, arguments: args);
 
   @override
   State<StudentRegistrationPage> createState() =>
@@ -38,11 +41,18 @@ class _StudentRegistrationPageState extends State<StudentRegistrationPage> {
 
   @override
   Widget build(BuildContext context) {
+    final args = ModalRoute.of(context)?.settings.arguments as StudentModel?;
+    controller.studentName = args?.name ?? "";
+    controller.dateOfBirth = args?.birthdate ?? "";
+    controller.cpf = args?.cpf ?? "";
+    controller.ra = args?.academicRecord ?? "";
+    controller.email = args?.email ?? "";
+
     return Scaffold(
       appBar: AppBarOceanBlueColorWidget(
         title: AppStrings.addStudentString,
         showIconArrow: true,
-        returnPage: () => {HomePage.push("args")},
+        returnPage: () => {HomePage.push()},
       ),
       body: Padding(
         padding: const EdgeInsets.all(AppDimens.defaultMarginDimension),
@@ -61,6 +71,7 @@ class _StudentRegistrationPageState extends State<StudentRegistrationPage> {
                 ),
                 const SizedBox(height: textBoxSpacing),
                 RectangularTextFieldWidget(
+                  textController: controller.studentName,
                   label: "${AppStrings.studentNameString}*",
                   onChanged: (text) => controller.getStudentName(text),
                   validator: (value) =>
@@ -68,11 +79,13 @@ class _StudentRegistrationPageState extends State<StudentRegistrationPage> {
                 ),
                 const SizedBox(height: textBoxSpacing),
                 RectangularTextFieldWidget(
+                  textController: controller.dateOfBirth,
                   label: AppStrings.birthdateString,
                   onChanged: (text) => controller.getDateOfBirth(text),
                 ),
                 const SizedBox(height: textBoxSpacing),
                 RectangularTextFieldWidget(
+                  textController: controller.cpf,
                   label: "${AppStrings.cpfString}*",
                   onChanged: (text) => controller.getCpf(text),
                   validator: (value) =>
@@ -80,8 +93,9 @@ class _StudentRegistrationPageState extends State<StudentRegistrationPage> {
                 ),
                 const SizedBox(height: textBoxSpacing),
                 RectangularTextFieldWidget(
-                  label: "${AppStrings.academicRecordString}*",
+                  textController: controller.ra,
                   onChanged: (text) => controller.getRA(text),
+                  label: "${AppStrings.academicRecordString}*",
                   validator: (value) =>
                       TextFieldValidator.validateRequired(value),
                 ),
@@ -99,32 +113,33 @@ class _StudentRegistrationPageState extends State<StudentRegistrationPage> {
                   height: textBoxSpacing,
                 ),
                 RectangularTextFieldWidget(
+                  textController: controller.email,
                   label: "${AppStrings.emailString}*",
                   onChanged: (text) => controller.getEmail(text),
                   validator: (value) =>
                       TextFieldValidator.validateRequired(value),
                 ),
                 const SizedBox(height: textBoxSpacing),
-                Observer(
-                  builder: (context) {
-                    return RoundedButtonWidget(
-                      text: !controller.isLoading ? Text(
-                        AppStrings.addString,
-                        style: AppTextStyles.interWhiteFontStyle(
-                          fontSize: AppDimens.defaultMarginDimension,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ) : const CircularProgressWhiteColorWidget(),
-                      onPressed: () async {
-                        if (_formKey.currentState!.validate()) {
-                          await controller.createStudent(context);
-                          if (controller.errorMessage.isEmpty) {}
-                        }
-                      },
-                      buttonColor: AppColors.oceanBlueColor,
-                    );
-                  }
-                ),
+                Observer(builder: (context) {
+                  return RoundedButtonWidget(
+                    text: !controller.isLoading
+                        ? Text(
+                            AppStrings.addString,
+                            style: AppTextStyles.interWhiteFontStyle(
+                              fontSize: AppDimens.defaultMarginDimension,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          )
+                        : const CircularProgressWhiteColorWidget(),
+                    onPressed: () async {
+                      if (_formKey.currentState!.validate()) {
+                        await controller.createStudent(context);
+                        if (controller.errorMessage.isEmpty) {}
+                      }
+                    },
+                    buttonColor: AppColors.oceanBlueColor,
+                  );
+                }),
               ],
             ),
           ),
@@ -133,5 +148,3 @@ class _StudentRegistrationPageState extends State<StudentRegistrationPage> {
     );
   }
 }
-
-
