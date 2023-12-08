53e239de5528f27b78bbf5336eae036a640ba0eb
Elvis <elvis@ezsolucoes.com.br>
2023-12-08 12:24:24 -0300

    feature(TextFieldValidator): adiciona o metodo validateDate.

--- lib/utils/text_field_validator.dart
+++ lib/utils/text_field_validator.dart
@@ -45,4 +45,38 @@ class TextFieldValidator {
 
     return null;
   }
+
+  static String? validateDate(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Por favor, preencha o campo de data.';
+    }
+
+    final cleanDate = value.replaceAll(RegExp(r'\D'), '');
+
+    if (cleanDate.length != 8) {
+      return 'Por favor, insira uma data válida no formato dd/MM/yyyy.';
+    }
+
+    final day = int.tryParse(cleanDate.substring(0, 2));
+    final month = int.tryParse(cleanDate.substring(2, 4));
+    final year = int.tryParse(cleanDate.substring(4, 8));
+
+    if (day == null || month == null || year == null) {
+      return 'Por favor, insira uma data válida.';
+    }
+
+    try {
+      final DateTime date = DateTime(year, month, day);
+      // Verifica se o ano, mês e dia correspondem ao informado pelo usuário
+      if (date.year != year || date.month != month || date.day != day) {
+        return 'Por favor, insira uma data válida.';
+      }
+    } catch (e) {
+      // Trata erros ao criar o objeto DateTime
+      return 'Por favor, insira uma data válida.';
+    }
+
+    return null;
+  }
+
 }
