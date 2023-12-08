7ea698f1f2fc6cc411027be17cee3e15d730b972
Elvis <elvis@ezsolucoes.com.br>
2023-12-07 21:54:56 -0300

    feature(LoginPage): adiciona os validadores nos campos.

--- lib/pages/login_page/login_page.dart
+++ lib/pages/login_page/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:challenge_mobile_developer/utils/text_field_validator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
@@ -11,7 +12,7 @@ import '../../controller/authentication_controller.dart';
 import '../../widgets/circular_progress_widgets/circular_progress_white_color_widget.dart';
 import 'components/login_widgets/login_widget.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
   static const String routePath = AppRoutes.appLoginPage;
@@ -22,28 +23,44 @@ class LoginPage extends StatelessWidget {
       Modular.to.pushNamed(routePath, arguments: args);
 
   @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  @override
   Widget build(BuildContext context) {
     final controller = Modular.get<AuthenticationController>();
     return Scaffold(
       backgroundColor: AppColors.pureWhiteColor,
-      body: LoginWidget(
-        getUser: (String? text) => controller.setUser(text!),
-        getPassword: (String? text) => controller.setPassword(text!),
-        forgotUsername: () => ShowMessage.showSuccessMessage(
-            context, AppStrings.forgotUsernameString),
-        forgotPassword: () => ShowMessage.showSuccessMessage(
-            context, AppStrings.forgotPasswordString),
-        login: () => controller.createUser(context),
-        register: () =>
-            ShowMessage.showSuccessMessage(context, AppStrings.registerString),
-        loading: Observer(builder: (context) {
-          return controller.isLoading
-              ? const CircularProgressWhiteColorWidget()
-              : const Text(
-                  AppStrings.loginString,
-                  style: TextStyle(color: AppColors.pureWhiteColor),
-                );
-        }),
+      body: Form(
+        key: _formKey,
+        child: LoginWidget(
+          getUser: (String? text) => controller.setUser(text!),
+          getPassword: (String? text) => controller.setPassword(text!),
+          forgotUsername: () => ShowMessage.showSuccessMessage(
+              context, AppStrings.forgotUsernameString),
+          forgotPassword: () => ShowMessage.showSuccessMessage(
+              context, AppStrings.forgotPasswordString),
+          validatorEmail: (value) => TextFieldValidator.validateEmail(value),
+          validatorPassword: (value) => TextFieldValidator.validatePassword(value),
+          login: () {
+            if (_formKey.currentState!.validate()) {
+              controller.createUser(context);
+            }
+          },
+          register: () => ShowMessage.showSuccessMessage(
+              context, AppStrings.registerString),
+          loading: Observer(builder: (context) {
+            return controller.isLoading
+                ? const CircularProgressWhiteColorWidget()
+                : const Text(
+                    AppStrings.loginString,
+                    style: TextStyle(color: AppColors.pureWhiteColor),
+                  );
+          }),
+        ),
       ),
     );
   }
